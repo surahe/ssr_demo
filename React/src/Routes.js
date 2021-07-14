0fa955fa6788174f52a4c7fd3397f660dad5eda9
Sura <375262986@qq.com>
2021-07-14 18:37:42 +0800

    添加嵌套路由

--- React/src/Routes.js
+++ React/src/Routes.js
@@ -1,18 +1,25 @@
 import Home from './containers/Home';
 import Login from './containers/Login'
+import App from './App'
 
 export default [
   {
-    path: "/",
-    component: Home,
-    exact: true,
-    loadData: Home.loadData,//服务端获取异步数据的函数
-    key: 'home'
-  },
-  {
-    path: '/login',
-    component: Login,
-    exact: true,
-    key: 'login'
+    path: '/',
+    component: App,
+    routes: [
+      {
+        path: "/",
+        component: Home,
+        exact: true,
+        loadData: Home.loadData,
+        key: 'home',
+      },
+      {
+        path: '/login',
+        component: Login,
+        exact: true,
+        key: 'login',
+      }
+    ]
   }
-];
+]
